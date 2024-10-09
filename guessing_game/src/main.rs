e6b0aee1cf62f74aabaa0f6a382bbc4d15b25053
Sonia Parmar <soniaparmar24@gmail.com>
2024-10-09 19:38:02 +0200

    run the guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,27 @@
+use rand::Rng; //random number gengerator
+use std::cmp::Ordering;
+use std::io; //input/output library
+
+fn main() {  //main function
+    println!("Guess the number!");   //print the statement
+    let secret_number = rand::thread_rng().gen_range(1..=100); //generate a random number between
+    //1-100
+
+    println!("The secret number is: {secret_number}");  //print statement
+    println!("Please input your guess."); //print statement
+
+    let mut guess = String::new();  //define mutable variable
+    io::stdin()  //
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}"); //print statement
+
+    match guess.cmp(&secret_number) { //comparison of guess and secret number
+        Ordering::Less => print!("Too small!"),
+        Ordering::Greater => print!("Too big!"),
+        Ordering::Equal => print!("You win!"),
+    }
+} 
