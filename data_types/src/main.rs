940c1c3684ec8ceec2356635c7e75d9481ad0dc1
Sonia Parmar <soniaparmar24@gmail.com>
2024-10-11 21:49:22 +0200

    introduce data types

---
+++ data_types/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let _sum = 5+10;
+    let _difference = 95.5-4.3;
+    let _product = 4*30;
+    let _quotient = 56.7/32.2;
+    let _truncated = -5/3;
+    let _remainder = 43%5;
+    println!("remainder is {_remainder}");
+    
+    let _t = true;
+    let _f: bool = false;
+
+    let tup: (i32, f64, u8) = (500,6.4,1);
+    let (x,y,z) = tup;
+    println!("the calueo f x is: {x}");    
+    let _five_hundred = tup.0;
+    let _six_point_four = tup.1;
+    let _one = tup.2;
+
+
+
+}
+
