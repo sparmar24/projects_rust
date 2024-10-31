76da623623e2d491dced668fbb82cbc47de1b5ed
Sonia Parmar <soniaparmar24@gmail.com>
2024-10-31 14:19:12 +0100

    calculate area using tuple

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,13 +1,25 @@
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+//calculate area using tuple
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect = (30, 50);
+    println!("area of rectangle is {} squrare pixels",
+    area(rect)
+);
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
-    );
-}
-
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimesions: (u32, u32)) -> u32 {
+        dimesions.0 * dimesions.1
+    }
 }
