58eff9c7e67ae4cfcb273d6ec169e92f03027f72
Sonia Parmar <soniaparmar24@gmail.com>
2024-11-03 19:43:03 +0100

    add struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -13,13 +13,34 @@
 // }
 
 //calculate area using tuple
-fn main() {
-    let rect = (30, 50);
-    println!("area of rectangle is {} squrare pixels",
-    area(rect)
-);
+// fn main() {
+//     let rect = (30, 50);
+//     println!("area of rectangle is {} squrare pixels",
+//     area(rect)
+// );
 
-fn area(dimesions: (u32, u32)) -> u32 {
-        dimesions.0 * dimesions.1
-    }
+// fn area(dimesions: (u32, u32)) -> u32 {
+//         dimesions.0 * dimesions.1
+//     }
+// }
+
+//refactoring with structs
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 60,
+    };
+    println!(
+        "The area of the rectanlge is {} square pixels.",
+        area(&rect1)
+    );
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
